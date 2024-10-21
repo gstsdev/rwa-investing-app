b115ccbed7056effff17b6bc8004592da912bfba
Gustavo Sampaio <gustavosamirts@gmail.com>
2024-10-21 20:59:32 -0300

    chore(contract/hardhat): replace .env with hardhat config variables

--- contract/hardhat.config.ts
+++ contract/hardhat.config.ts
@@ -1,13 +1,12 @@
-import { HardhatUserConfig } from "hardhat/config";
+import { HardhatUserConfig, vars } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.27",
   networks: {
     polygonAmoy: {
-      url:
-        process.env.POLYGON_AMOY_RPC || "https://rpc-amoy.polygon.technology",
-      accounts: [process.env.ACCOUNT_PRIVATE_KEY!],
+      url: vars.get("POLYGON_AMOY_RPC", "https://rpc-amoy.polygon.technology"),
+      accounts: [vars.get("ACCOUNT_PRIVATE_KEY")],
     },
   },
 };
