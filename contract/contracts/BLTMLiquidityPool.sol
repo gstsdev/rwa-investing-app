88433c343bcf86c9277157fc48b9afd00410b116
Gustavo Sampaio <gustavosamirts@gmail.com>
2024-10-22 18:15:06 -0300

    feat(contract): add Liquidity Pool contract

---
+++ contract/contracts/BLTMLiquidityPool.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/interfaces/IERC20.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+
+contract BLTMLiquidityPool is Context, AccessControl {
+    bytes32 public constant OWNER_ROLE = keccak256("OWNER_ROLE");
+
+    IERC20 private BLTM;
+    IERC20 private USDC;
+    uint256 private exchangeRate;
+
+    constructor(
+        address erc20TokenAddress_,
+        address usdcTokenAddress_,
+        uint256 exchangeRate_
+    ) {
+        BLTM = IERC20(erc20TokenAddress_);
+        USDC = IERC20(usdcTokenAddress_);
+        exchangeRate = exchangeRate_;
+
+        _grantRole(OWNER_ROLE, _msgSender());
+    }
+}
