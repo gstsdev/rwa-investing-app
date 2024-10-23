f3e233c45c30f8cbdf7cd4ab7c5146b1135d475f
Gustavo Sampaio <gustavosamirts@gmail.com>
2024-10-23 13:21:18 -0300

    chore(web/app): show AccountInfo in home page

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,9 +1,11 @@
-import Image from "next/image";
+import AccountInfo from "@/components/AccountInfo";
 
 export default function Home() {
   return (
     <div className="min-h-screen">
-      <main className="flex flex-col gap-8 container items-center sm:items-start"></main>
+      <main className="flex flex-col gap-8 w-full max-w-[800px] items-center mx-auto p-5">
+        <AccountInfo />
+      </main>
     </div>
   );
 }
