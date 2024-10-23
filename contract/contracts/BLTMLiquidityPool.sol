dd66c24062e9d5f038c3118e08d980faa46349a1
Gustavo Sampaio <gustavosamirts@gmail.com>
2024-10-22 21:32:23 -0300

    feat(contract/BLTMLiquidityPool): exchange USDC for BLTM

--- contract/contracts/BLTMLiquidityPool.sol
+++ contract/contracts/BLTMLiquidityPool.sol
@@ -4,6 +4,10 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/interfaces/IERC20.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 
+interface Mintable {
+    function mint(address to, uint256 amount) external;
+}
+
 contract BLTMLiquidityPool is Context, AccessControl {
     bytes32 public constant OWNER_ROLE = keccak256("OWNER_ROLE");
 
@@ -28,5 +32,29 @@ contract BLTMLiquidityPool is Context, AccessControl {
         uint256 exchangeRate_
     ) public onlyRole(OWNER_ROLE) {
         exchangeRate = exchangeRate_;
+    }
+
+    function exchangeUsdcForToken(uint256 value) public {
+        require(
+            USDC.allowance(_msgSender(), address(this)) >= value,
+            "USDC allowance too low"
+        );
+
+        _safeTransferFrom(USDC, _msgSender(), address(this), value);
+
+        uint256 tokenAmount = value * exchangeRate;
+
+        Mintable(address(BLTM)).mint(_msgSender(), tokenAmount);
+    }
+
+    function _safeTransferFrom(
+        IERC20 token,
+        address sender,
+        address recipient,
+        uint256 amount
+    ) private {
+        bool sent = token.transferFrom(sender, recipient, amount);
+
+        require(sent, "Token transfer failed");
     }
 }
